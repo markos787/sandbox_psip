11f167a0f8b88f33d17849f79bcac8320ba0627c
markos787 <149382126+markos787@users.noreply.github.com>
2023-11-27 14:51:49 +0100

    chore: gui_update-map

--- dane.py
+++ dane.py
@@ -1,12 +1,12 @@
-users_list:list = [
-    {'name': 'Marek', 'nick': 'Wise', 'posts': 5},
-    {'name': 'Marek', 'nick': 'wysz', 'posts': 100},
-    {'name': 'Adam', 'nick': 'dobry', 'posts': 1},
-    {'name': 'Bartek', 'nick': 'barto', 'posts': 1},
-    {'name': 'Antek', 'nick': 'Macio', 'posts': 30},
-    {'name': 'Andrew', 'nick': 'Tate', 'posts': 556},
-    {'name': 'Tristan', 'nick': 'Tati', 'posts': 34},
-    {'name': 'Tomasz', 'nick': 'Kocu', 'posts': 32},
-    {'name': 'Karol', 'nick': 'Laska', 'posts': 69},
-    {'name': 'Agata', 'nick': 'Agaciu', 'posts': 5283547},
+users_list: list = [
+    {'city': 'Zamość', 'name': 'Marek', 'nick': 'Wise', 'posts': 5},
+    {'city': 'Lublin', 'name': 'Marek', 'nick': 'wysz', 'posts': 100},
+    {'city': 'Opoczno', 'name': 'Adam', 'nick': 'dobry', 'posts': 1},
+    {'city': 'Licheń Stary', 'name': 'Bartek', 'nick': 'barto', 'posts': 1},
+    {'city': 'Brodnica', 'name': 'Antek', 'nick': 'Macio', 'posts': 30},
+    {'city': 'Kuryłówka', 'name': 'Andrew', 'nick': 'Tate', 'posts': 556},
+    {'city': 'Warszawa', 'name': 'Tristan', 'nick': 'Tati', 'posts': 34},
+    {'city': 'Mińsk_Mazowiecki', 'name': 'Tomasz', 'nick': 'Kocu', 'posts': 32},
+    {'city': 'Koszalin', 'name': 'Karol', 'nick': 'Laska', 'posts': 69},
+    {'city': 'Hrubieszów', 'name': 'Agata', 'nick': 'Agaciu', 'posts': 5283547},
 ]
--- notatki.py
+++ notatki.py
@@ -16,37 +16,3 @@
 # for user in users_list:
 #     print(user)
 
-import requests
-from bs4 import BeautifulSoup
-
-city=['Warszawa','Brodnica', 'Iława']
-
-def get_coordinates(city:str)->list[float,float]:
-    # pobieranie strony internetowej
-    adres_url=f'https://pl.wikipedia.org/wiki/{city}'
-
-    response=requests.get(url=adres_url) #zwraca obiekt, wywołany jest status
-    response_html=BeautifulSoup(response.text, 'html.parser') #zwraca tekst kodu strony internetowej, zapisany w html
-
-    #pobieranie współrzędnych
-    response_html_lat=response_html.select('.latitude')[1].text #kropka oznacza klasę, do ID odwołujemy sie przez #
-    response_html_lat=float(response_html_lat.replace(',','.'))
-
-    response_html_long=response_html.select('.longitude')[1].text #kropka oznacza klasę, do ID odwołujemy sie przez #
-    response_html_long=float(response_html_long.replace(',','.'))
-
-    return [response_html_lat,response_html_long]
-
-# for item in city:
-#     print(get_coordinates(item))
-
-
-###RYSOWANIE MAPY
-import folium
-
-#zwrócić mapę z pinezką odnoszącą się do wskazanego użytkownika podanego z klawiatury
-#zwrócić mapę z wszystkimi użytkownikami z danej listy (znajomymi)
-map=folium.Map(location=[52.3,21.0],tiles='OpenStreetMap', zoom_start=7) #location to miejsce wycentrowania mapy
-for item in city:
-    folium.Marker(location=get_coordinates(city=item),popup='Why are you gay?').add_to(map)
-map.save('mapka.html')
