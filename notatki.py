4da9f44bcd2eb6130e4d7543c96fe84f85dd189b
markos787 <149382126+markos787@users.noreply.github.com>
2023-11-27 13:15:47 +0100

    chore: get_coordinates

--- notatki.py
+++ notatki.py
@@ -1,19 +1,41 @@
+# from dane import users_list
+#
+# #nick_of_user = input('podaj nick użytkownika do modyfikacji')
+# #print(nick_of_user)
+# def update_user(users_list: list[dict, dict]) -> None:
+#     nick_of_user = input('podaj nick użytkownika do modyfikacji')
+#     print(nick_of_user)
+#     for user in users_list:
+#         if user['nick'] == nick_of_user:
+#             print('Znaleziono')
+#             user['name'] = input('podaj nowe imie: ')
+#             user['nick'] = input('podaj nowe ksywe: ')
+#             user['posts'] =int(input('podaj liczbw postów: '))
+#
+# update_user(users_list)
+# for user in users_list:
+#     print(user)
 
-from dane import users_list
+import requests
+from bs4 import BeautifulSoup
 
-#nick_of_user = input('podaj nick użytkownika do modyfikacji')
-#print(nick_of_user)
-def update_user(users_list: list[dict, dict]) -> None:
-    nick_of_user = input('podaj nick użytkownika do modyfikacji')
-    print(nick_of_user)
-    for user in users_list:
-        if user['nick'] == nick_of_user:
-            print('Znaleziono')
-            user['name'] = input('podaj nowe imie: ')
-            user['nick'] = input('podaj nowe ksywe: ')
-            user['posts'] =int(input('podaj liczbw postów: '))
+item=['Warszawa','Brodnica']
 
-update_user(users_list)
-for user in users_list:
-    print(user)
+def get_coordinates(city:str)->list[float,float]:
+    # pobieranie strony internetowej
+    adres_url=f'https://pl.wikipedia.org/wiki/{city}'
 
+    response=requests.get(url=adres_url) #zwraca obiekt, wywołany jest status
+    response_html=BeautifulSoup(response.text, 'html.parser') #zwraca tekst kodu strony internetowej, zapisany w html
+
+    #pobieranie współrzędnych
+    response_html_lat=response_html.select('.latitude')[1].text #kropka oznacza klasę, do ID odwołujemy sie przez #
+    response_html_lat=float(response_html_lat.replace(',','.'))
+
+    response_html_long=response_html.select('.longitude')[1].text #kropka oznacza klasę, do ID odwołujemy sie przez #
+    response_html_long=float(response_html_long.replace(',','.'))
+
+    return [response_html_lat,response_html_long]
+
+for item in item:
+    print(get_coordinates(item))
